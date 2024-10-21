af03d9d2e8e40f5e2544798095aab702bdc5f063
Douha Assafiri <61601002+douhaassafiri@users.noreply.github.com>
2024-10-21 11:04:34 +1100

    Add files via upload

---
+++ phpenhancements.php
@@ -0,0 +1,12 @@
+<div class="grid-item">
+    <p>A manager is prompted to log in before accessing the Manage page.
+        <br>We can use username "admin" and password "admin" to log in.
+    </p><br>
+    <img src="images/login.png" class="screenshot" alt="login">
+</div>
+<div class="grid-item">
+    <p>We added the option to register as a new user.
+        <br>the new account can be used to login after registering.
+    </p><br>
+    <img src="images/register.png" class="screenshot" alt="register">
+</div>
